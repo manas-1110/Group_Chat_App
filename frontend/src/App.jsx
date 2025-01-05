ac404bc31ea38f9ae9c841b1786d28dad93364c5
manas-1110 <manas11102004@gmail.com>
2025-01-05 11:31:37 +0000

    basic traversing possible

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Route } from "react-router-dom/cjs/react-router-dom.min";
+import HomePage from "./Pages/homePage";
+import ChatPage from "./Pages/chatPage";
 
 function App() {
     const [count, setCount] = useState(0);
-
-    return <Button colorScheme="teal">Click me</Button>;
+    return (
+        <div className="App">
+            <Route path="/" component={HomePage} exact />
+            <Route path="/chats" component={ChatPage} />
+        </div>
+    );
 }
 
 export default App;
