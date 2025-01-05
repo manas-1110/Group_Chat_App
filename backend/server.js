4bb3f7c991afd6b7ed1777f5c111ba07fc85893a
manas-1110 <manas11102004@gmail.com>
2025-01-05 10:38:59 +0000

    added chakra UI

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { chats } = require("./chat_data/data");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+app.get("/api/chats", (req, res) => {
+    res.json(chats);
+    // res.send("API is running...");
+});
+
+app.get("/api/chats/:id", (req, res) => {
+    const chat = chats.find((c) => c._id === req.params.id);
+    // console.log(req.params.id);
+    res.json(chat);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, console.log("Server is running on port", PORT));
