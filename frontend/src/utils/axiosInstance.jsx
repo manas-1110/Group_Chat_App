043e2fa4aeb1a22ea8cafd53e4e67457c7f0b441
Manas <manas11102004@gmail.com>
2025-01-17 12:48:26 +0530

    file save2

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL:
-        "https://group-chat-app-vnh5-fhx1apdnl-manas-thakurs-projects-690151d7.vercel.app/", // Replace with production URL later
+        "https://group-chat-app-vnh5-q6xw9ptr0-manas-thakurs-projects-690151d7.vercel.app/", // Replace with production URL later
 });
 
 export default axiosInstance;
