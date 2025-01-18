163e6e6e01b059a38398771b50bca07c8edfc4a1
Manas <manas11102004@gmail.com>
2025-01-18 13:38:38 +0530

    d

--- frontend/src/utils/axiosInstance.jsx
+++ frontend/src/utils/axiosInstance.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://group-chat-app-vnh5.vercel.app/", // Replace with production URL later
+    baseURL: "https://group-chat-app-vnh5.vercel.app",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 export default axiosInstance;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { loadEnv } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     server: {
         proxy: {
             "/api": {
-                target: "https://group-chat-app-vnh5.vercel.app/",
-                changeOrigin: true,
+                target: "https://group-chat-app-vnh5.vercel.app",
             },
         },
     },
